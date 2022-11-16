b6e7c09552dc0f74e101bc29de1fe0e4b9cd4fc7
minkyoung kim <minry20415@naver.com>
2022-11-16 20:46:46 +0900

    [add] 221116 articles api practice

---
+++ src/main/java/com/springboot/articles/SpringbootArticlesApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootArticlesApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootArticlesApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/articles/SpringbootArticlesApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.articles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootArticlesApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
